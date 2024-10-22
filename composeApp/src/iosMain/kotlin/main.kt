4360acc5a26600d57936bd371cd8fe59c990c5d2
goplen <nikita120805@mail.ru>
2024-10-22 17:47:29 +0500

    upgrade compose version to 1.7.0

--- composeApp/src/iosMain/kotlin/main.kt
+++ composeApp/src/iosMain/kotlin/main.kt
@@ -2,7 +2,8 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.oilpay.mobile.App
 import com.oilpay.mobile.features.root.domain.RootComponent
 import com.oilpay.mobile.features.root.screen.RootScreen
+import oilpay.mobile.foundation.theme.OilPayTheme
 import platform.UIKit.UIViewController
 
 fun MainViewController(rootComponent: RootComponent): UIViewController =
-    ComposeUIViewController { RootScreen(rootComponent) }
+    ComposeUIViewController { OilPayTheme { RootScreen(rootComponent) } }
